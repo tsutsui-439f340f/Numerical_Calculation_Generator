075cab2ac471c75c905b553d430dce6c5ae9e39a
tsutsui6Electronics <55880071+tsutsui6Electronics@users.noreply.github.com>
2022-01-20 12:37:41 +0900

    Add files via upload

--- src/HTML2PDF.py
+++ src/HTML2PDF.py
@@ -10,25 +10,15 @@ import base64
 n_print=10
 cwd = os.getcwd()
 
-"""
-while True:
-    if not os.path.exists(SAVE_PATH):
-        os.makedirs(SAVE_PATH)
-        break
-    else:
-        SAVE_PATH=SAVE_PATH+'_{}'.format(f)
-        f+=1
-"""
-
+print(len(sys.argv))
 if len(sys.argv)==1:
     print("USAGE: {} (template_file) (output_path) (num_print)".format(sys.argv[0]))
     exit()
 elif len(sys.argv)==2:
-    #print("chrome-driver is not found.")
     print("output path is not found.")
     exit()
 elif len(sys.argv)>3:
-    n_print=sys.argv[3]
+    n_print=int(sys.argv[3])
 
 
 SAVE_PATH=sys.argv[2]
@@ -45,19 +35,15 @@ print(TEMP_FILE)
 
 def save_to_pdf(driver, file_path):
     parameters = {
-                    "printBackground": True, # 背景画像を印刷
-                    "paperWidth": 8.27, # A4用紙の横 210mmをインチで指定
-                    "paperHeight": 11.69, # A4用紙の縦 297mmをインチで指定
-                    # "displayHeaderFooter": True, # 印刷時のヘッダー、フッターを
+                    "printBackground": True,
+                    "paperWidth": 8.27,
+                    "paperHeight": 11.69, 
                 }
-                    # Chrome Devtools Protocolコマンドを実行し、取得できるBase64形式のPDFデータをデコードしてファイルに保存
+                    
     pdf_base64 = driver.execute_cdp_cmd("Page.printToPDF", parameters)
     pdf = base64.b64decode(pdf_base64["data"])
     with open(file_path, 'bw') as f:
         f.write(pdf)
-
-
-
 
 options = webdriver.ChromeOptions() 
 options.add_argument('--headless')
